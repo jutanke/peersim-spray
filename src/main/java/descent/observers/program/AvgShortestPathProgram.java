257d6d5a871f1f93a62b505b9a685094b4ebf8dc
skoazell <brice.nedelec@gmail.com>
2015-05-11 21:31:47 +0200

    measurements done on 1 5 10 50 100 500 1000 etc

--- src/main/java/descent/observers/program/AvgShortestPathProgram.java
+++ src/main/java/descent/observers/program/AvgShortestPathProgram.java
@@ -1,48 +1,53 @@
 package descent.observers.program;
 
-import descent.observers.DictGraph;
-import descent.observers.ObserverProgram;
-import peersim.core.CommonState;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import peersim.core.CommonState;
+import descent.observers.DictGraph;
+import descent.observers.ObserverProgram;
 
 /**
  * Created by julian on 11/05/15.
  */
 public class AvgShortestPathProgram implements ObserverProgram {
 
+	int lastSize = 0;
+	int tick = 0;
 
-    int lastSize = 0;
-    int tick = 0;
+	public void tick(long currentTick, DictGraph observer) {
 
-    public void tick(long currentTick, DictGraph observer) {
+		if (this.lastSize < observer.size()) {
+			this.tick += 1;
+			if (this.tick >= 20) {
+				this.tick = 0;
+				this.lastSize = observer.size();
+				if (observer.size() % ((Math.pow(10,
+						Math.ceil(Math.log10(observer.size()))))/2) == 0) {
 
-        if (this.lastSize < observer.size()) {
-            this.tick += 1;
-            if (this.tick >= 20) {
-                this.tick = 0;
-                this.lastSize = observer.size();
-                final List<Long> ids = new ArrayList<Long>(observer.nodes.keySet());
+					final List<Long> ids = new ArrayList<Long>(
+							observer.nodes.keySet());
 
-                double total = 0.0;
-                total += observer.avgReachablePaths(ids.get(CommonState.r.nextInt(ids.size()))).avg;
+					double total = 0.0;
+					total += observer.avgReachablePaths(ids.get(CommonState.r
+							.nextInt(ids.size()))).avg;
 
-                total += observer.avgReachablePaths(ids.get(CommonState.r.nextInt(ids.size()))).avg;
+					total += observer.avgReachablePaths(ids.get(CommonState.r
+							.nextInt(ids.size()))).avg;
 
-                total += observer.avgReachablePaths(ids.get(CommonState.r.nextInt(ids.size()))).avg;
+					total += observer.avgReachablePaths(ids.get(CommonState.r
+							.nextInt(ids.size()))).avg;
 
-                System.out.println(observer.size() + " " + total/3);
+					System.out.println(observer.size() + " " + total / 3);
+				}
 
+			}
+		}
 
-            }
-        }
+	}
 
+	public void onLastTick(DictGraph observer) {
 
-    }
-
-    public void onLastTick(DictGraph observer) {
-
-    }
+	}
 
 }
