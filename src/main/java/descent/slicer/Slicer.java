80dc3f8c864e0523de72708d061c550505619cd5
chat-wane <grumpy.chat.wane@gmail.com>
2017-06-23 17:25:19 +0200

    start stamping

--- src/main/java/descent/slicer/Slicer.java
+++ src/main/java/descent/slicer/Slicer.java
@@ -7,21 +7,44 @@ import descent.rps.IPeerSampling;
 import descent.spray.SprayPartialView;
 import descent.tman.TMan;
 import descent.tman.TManPartialView;
+import itc.Stamp;
+import peersim.config.Configuration;
 import peersim.core.CommonState;
 import peersim.core.Node;
 
+/**
+ * Overlay network building (i) slices depending on the rank and (ii) providing
+ * the right to stamp to a portion of slices |P|/X.
+ */
 public class Slicer extends TMan {
 
 	private static boolean SWAP = true;
+	private static final String PAR_X = "x";
+	protected static Double X = 3.;
+
+	public Stamp stamp;
+	public boolean isWriter = false;
 
 	public Slicer(String prefix) {
 		super(prefix);
 		this.descriptor = new RankDescriptor();
+		Slicer.X = Configuration.getDouble(prefix + "." + Slicer.PAR_X);
 	}
 
 	public Slicer() {
 		super();
 		this.descriptor = new RankDescriptor();
+	}
+
+	@Override
+	public void onSubscription(Node origin) {
+		if (this.partialView.size() == 0) {
+			((RankDescriptor) this.descriptor).setRank(0);
+			this.stamp = new Stamp();
+			this.isWriter = true;
+		}
+
+		super.onSubscription(origin);
 	}
 
 	public void periodicCall() {
@@ -33,7 +56,7 @@ public class Slicer extends TMan {
 
 		// #1 initialize descriptor based on Spray
 		if (this.age >= Math.max(this.partialView.size() / this.A, 6) && !((RankDescriptor) this.descriptor).isSet()) {
-			((RankDescriptor) this.descriptor).setRank((int) Math.floor(this.partialView.size() / this.A) - 1);
+			((RankDescriptor) this.descriptor).setRank((int) Math.floor(this.partialView.size() / this.A));
 		}
 
 		// #2 see if a swap of rank is needed
@@ -74,6 +97,23 @@ public class Slicer extends TMan {
 
 		// #3 exchange views
 		super.periodicCall();
+
+		// #4 getting a stamp
+		if (((RankDescriptor) this.descriptor).rank <= this.partialView.size() / X) {
+			boolean found = false;
+			ArrayList<Node> candidates = new ArrayList<Node>(this.partialViewTMan);
+			candidates.addAll(this.partialView.getPeers());
+			Integer i = 0;
+			while (!found && i < candidates.size()) {
+				Node node = candidates.get(i);
+				Slicer slicer = (Slicer) node.getProtocol(Slicer.pid);
+				RankDescriptor slicerDescriptor = (RankDescriptor) slicer.descriptor;
+				if (slicerDescriptor.rank < ((RankDescriptor) this.descriptor).rank && slicer.isWriter) {
+					found = true;
+				}
+				++i;
+			}
+		}
 	}
 
 	@Override
